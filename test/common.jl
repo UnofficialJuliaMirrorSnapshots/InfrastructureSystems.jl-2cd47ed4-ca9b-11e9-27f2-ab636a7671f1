b9c719d81803f2a343fa9916dce3fb04b5bd3cb8
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-25 12:34:34 +0000

    Snapshot of branch master taken on 2019-09-25T08:34:33.071-04:00 from "https://github.com/UnofficialJuliaMirror/InfrastructureSystems.jl-2cd47ed4-ca9b-11e9-27f2-ab636a7671f1"

--- test/common.jl
+++ test/common.jl
@@ -1,24 +1,14 @@
 
-struct TestComponent <: InfrastructureSystemsType
-    name::AbstractString
-    val::Int
-    internal::InfrastructureSystemsInternal
-end
-
-function TestComponent(name, val, internal=InfrastructureSystemsInternal())
-    return TestComponent(name, val, internal)
-end
-
 function create_system_data(; with_forecasts=false)
-    data = SystemData()
+    data = IS.SystemData()
 
     name = "Component1"
-    component = TestComponent(name, 5)
-    add_component!(data, component)
+    component = IS.TestComponent(name, 5)
+    IS.add_component!(data, component)
 
     if with_forecasts
         file = joinpath(FORECASTS_DIR, "ComponentsAsColumnsNoTime.json")
-        add_forecasts!(data, make_forecasts(data, file, IS))
+        IS.add_forecasts!(data, IS.make_forecasts(data, file, IS))
 
         forecasts = get_all_forecasts(data)
         @assert length(forecasts) > 0
@@ -28,6 +18,5 @@ function create_system_data(; with_forecasts=false)
 end
 
 function get_all_forecasts(data)
-    return collect(iterate_forecasts(data))
+    return collect(IS.iterate_forecasts(data))
 end
-
--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,5 +1,13 @@
 
 @testset "Test utility functions" begin
-    concrete_types = IS.get_all_concrete_subtypes(InfrastructureSystemsType)
+    concrete_types = IS.get_all_concrete_subtypes(IS.InfrastructureSystemsType)
     @test length([x for x in concrete_types if isconcretetype(x)]) == length(concrete_types)
+end
+
+@testset "Test strip_module_name" begin
+    @test IS.strip_module_name("PowerSystems.HydroDispatch") == "HydroDispatch"
+
+    @test IS.strip_module_name(
+        "InfrastructureSystems.Deterministic{PowerSystems.HydroDispatch}") ==
+        "Deterministic{PowerSystems.HydroDispatch}"
 end
